bfa4446f04251687c410360fb3241364dba84448
alfian <alfian000000@gmail.com>
2019-11-17 19:16:49 +0700

    Views Standart

--- resources/views/outlets/create.blade.php
+++ resources/views/outlets/create.blade.php
@@ -87,7 +87,7 @@ Buat Akun Petani
     function updateMarker(lat, lng) {
         marker
         .setLatLng([lat, lng])
-        .bindPopup("Your location :  " + marker.getLatLng().toString())
+        .bindPopup("Lokasi Anda :  " + marker.getLatLng().toString())
         .openPopup();
         return false;
     };
